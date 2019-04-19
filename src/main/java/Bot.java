89983e0bd943e8aa3d003a617e45c4a383a13bd2
KrupoderovMikhail <mishka.crash@gmail.com>
2019-04-20 00:38:05 +0500

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,113 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private static final String USERNAME = "FriendlyAssistantTestBot";
+    private static final String TOKEN = "849232261:AAEoCCa4qNFalkDqpMG-Znio-tTe_Adk2_E";
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param message
+     * @param text
+     */
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true); // Включить возможность разметки
+        sendMessage.setChatId(message.getChatId().toString()); // определяем id чата, в который нужно отправить сообщение
+        sendMessage.setReplyToMessageId(message.getMessageId()); // определяем id сообщения, на которое нужно ответить
+        sendMessage.setText(text);
+        try {
+            setButtons(sendMessage); // добавить кнопки
+            execute(sendMessage); // добавить сообщение
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод для приема сообщений
+     * @param update Содержит сообщение о пользовании
+     */
+    @Override
+    public synchronized void onUpdateReceived(Update update) {
+        Model model = new Model();
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/start":
+                    sendMsg(message, "Привет!");
+                    break;
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                case "/weather":
+                    sendMsg(message, "В каком городе вы хотите узнать прогноз погоды?");
+                    break;
+                case "Привет":
+                    sendMsg(message, "Маме своей привет напиши кожаный ублюдок!");
+                    break;
+                default:
+                    try {
+                            sendMsg(message, Weather.getWeather(message.getText(), model));
+                    } catch (IOException e) {
+                        sendMsg(message, "Город не найден!");
+                    }
+            }
+        }
+    }
+
+
+    public void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup); // связать сообщения с клавиатурой
+        replyKeyboardMarkup.setSelective(true); // показывать клавиатуру определенным пользователям
+        replyKeyboardMarkup.setResizeKeyboard(true); // "подгонка" клавиатуры
+        replyKeyboardMarkup.setOneTimeKeyboard(false); // не скрывать клавиатуру
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+//        List<KeyboardRow> keyboardRowList2 = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow(); // Строка клавиатуры (1-я)
+//        KeyboardRow keyboardSecondRow = new KeyboardRow(); // Строка клавиатуры (2-я)
+
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/settings"));
+        keyboardFirstRow.add(new KeyboardButton("/weather"));
+
+
+        keyboardRowList.add(keyboardFirstRow); // добавить кнопку в список
+        replyKeyboardMarkup.setKeyboard(keyboardRowList); // добавить лист в массив кнопок
+//        replyKeyboardMarkup.setKeyboard(keyboardRowList2);
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации
+     * @return USERNAME
+     */
+    @Override
+    public String getBotUsername() {
+        return USERNAME;
+    }
+
+    /**
+     * Метод возвращает токен бота для связи с сервером Telegram
+     * @return TOKEN
+     */
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+}
---
+++ src/main/java/Weather.java
@@ -0,0 +1,43 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+    //https://api.openweathermap.org/data/2.5/weather?q=Chaykovskiy,RU&units=metric&appid=376b1c6d893122180eb3a004f4dad539
+
+    //376b1c6d893122180eb3a004f4dad539
+    public static String getWeather(String message, Model model) throws IOException {
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=376b1c6d893122180eb3a004f4dad539");
+
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = "";
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+
+        JSONObject object = new JSONObject(result); // объект json
+        model.setName(object.getString("name")); // достаем имя
+
+        JSONObject main = object.getJSONObject("main"); // создаем новый объект, выбирая данные "main" из object
+        model.setTemp(main.getDouble("temp")); // достаем температуру
+        model.setHumidity(main.getDouble("humidity"));
+
+        JSONArray getArray = object.getJSONArray("weather"); // создаем массив json
+        /* С помощью цикла помещаем данные из массива в объект obj */
+        for (int i = 0; i < getArray.length(); i++) {
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon(obj.get("icon").toString()); // помещаем иконку в модель
+            model.setMain(obj.get("main").toString()); // помещаем описание погоды в модель
+        }
+
+        return "City: " + model.getName() + "\n" +
+                "Temperature: " + model.getTemp() + "°C" + "\n" +
+                "Humidity: " + model.getHumidity() + "%" + "\n" +
+                "Main: " + model.getMain() + "\n" +
+                "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
+    }
+}
